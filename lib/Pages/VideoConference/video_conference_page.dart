45f3aaa61769ed3e96ae137522869254818ce54a
Hoang Pham <hoangpm6814@gmail.com>
2021-10-16 23:13:51 +0700

    Video Conference

---
+++ lib/Pages/VideoConference/video_conference_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class VideoConference extends StatefulWidget {
+  const VideoConference({Key key}) : super(key: key);
+
+  @override
+  _VideoConferenceState createState() => _VideoConferenceState();
+}
+
+class _VideoConferenceState extends State<VideoConference> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(title: Text("View Schedule")),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Container(
+            height: 50,
+            margin: EdgeInsets.symmetric(vertical: 40),
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Tutoring Meeting Room ',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "00:09",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            // color: Colors.black,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  // width: 100,
+                  height: 80,
+                  margin: EdgeInsets.symmetric(vertical: 100),
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    // border: Border.all(color: Colors.grey[600]),
+                    color: Color(0xFF26262b),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Lesson will be started after",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "15 : 44 : 01",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:lettutor/Pages/Schedule/view_list_schedule_page.dart';
+// import 'package:lettutor/Pages/Schedule/view_list_schedule_page.dart';
+import 'package:lettutor/Pages/VideoConference/video_conference_page.dart';
 // import 'package:lettutor/Pages/Schedule/view_list_schedule_page_history.dart';
 import 'package:lettutor/constants.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: ViewListSchedulePage(),
+      home: VideoConference(),
     );
   }
 }
