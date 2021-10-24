17a340175e4a915fc6ee3607d55b04abe2b3166e
Hoang Pham <hoangpm6814@gmail.com>
2021-10-24 22:28:21 +0700

    Search chat tutor page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lettutor/Pages/Welcome/welcome_page.dart';
+import 'package:lettutor/Pages/Chat/search_chat.dart';
 import 'package:lettutor/constants.dart';
 
 void main() => runApp(MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: WelcomePage(),
+      home: SearchChatPage(),
     );
   }
 }
