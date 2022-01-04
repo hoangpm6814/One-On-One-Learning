a63d991806b3e5480c18b717955dca5074a2f23f
hoangpm6814 <55278362+hoangpm6814@users.noreply.github.com>
2022-01-04 23:45:49 +0700

    Add logo to app

--- lib/screens/Splash/splash_screen.dart
+++ lib/screens/Splash/splash_screen.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Center(
-        child: Text('Loading...'),
+        child: Image.asset(
+          "assets/icons/app_logo_oneonone.png",
+          height: size.height * 0.4,
+        ),
       ),
     );
   }
